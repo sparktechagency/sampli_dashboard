6da6f71f392a59bf2d371ff36df988b04bdc82c7
shaharul siyam <shaharulsiyam@shaharuls-Mac-mini.local>
2026-02-08 11:55:37 +0600

    update

--- src/pages/sampler/SamplerHome/components/offer/components/OfferOrderDetails.jsx
+++ src/pages/sampler/SamplerHome/components/offer/components/OfferOrderDetails.jsx
@@ -104,8 +104,7 @@ const OfferOrderDetails = ({ setIsClicked, id }) => {
         formData.append("data", JSON.stringify(data));
 
         const res = await createReview(formData).unwrap();
-        // console.log(res)
-        // alert("wait")
+        // console.log(res)s
         setIsModalOpen(false);
         toast.success("Review submitted successfully!");
 
