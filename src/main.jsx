c87b948c4e9788bb673840e94ddad4b0f1ed1d8b
hosainahmeed <skyfal430@gmail.com>
2026-02-22 11:20:47 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/Routes.jsx";
-import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
 import { store } from "./Redux/main/store.js";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { router } from "./routes/Routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <HelmetProvider>
-        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API || ""}>
+        <GoogleOAuthProvider clientId="196782660552-0mh8i2e6l9sev66qjm9v6fhq4aqgiull.apps.googleusercontent.com">
           <Toaster position="top-center" />
           <RouterProvider router={router} />
         </GoogleOAuthProvider>
